852c964d967045fb22a64cc747d35bf5c169e745
s33btorr <s33btorr@uni-bonn.de>
2025-07-23 11:24:09 +0200

    Add descriptions to DRtest function.

--- ml_heterogeneity_project/analysis/forest/dr_tester.py
+++ ml_heterogeneity_project/analysis/forest/dr_tester.py
@@ -23,13 +23,45 @@ from train_test_split import split_data
 from causal_forest import generating_causal_forest
 
 
-def cf_drtest(outcome, treatment, covariates_names, question, 
+def cf_drtest(outcome, treatment, covariates, question, 
               model_outcome, model_treatment, 
               division_for_cate, percentage_test,
               n_trees, min_sample_per_leaf, max_samples, random_seed
               ):
 
-    x_train, x_test, d_train, d_test, y_train, y_test = split_data(treatment, outcome, covariates_names, percentage_test)
+    """ Saves graphs and print summary of the DRTest.
+
+    Args:
+        outcome (column): Column that represents the outcome.
+
+        treatment (column): Column that represents the treatment.
+
+        covariates (DataFrame): DataFrame of all columns representing the covariates.
+
+        model_outcome (object): Model that predicts the Outcome,
+        it can be selected from EconML documentation.
+
+        model_treatment (object): Model that predicts the Treatment,
+        it can be selected from EconML documentation.
+
+        division_for_cate (int): Number of divisions made in sample for testing.
+        If we choose 4, it will be divided into quartiles (which is the default).
+
+        percentage_test (float): Percentage of sample that we use for testing.
+        0.4 means 40% of sample is use for testing and the other 60% is use for training.
+
+        n_trees (int): Number of trees wanted in the forest.
+
+        min_sample_per_leaf (int): Minimum number of observations per leaf.
+
+        max_samples (float): In how much the data should be splitted when
+        doing the forest. The maximum allowed is 0.5 given Honesty.
+
+        random_seed (int): Seed for reproducibility.
+    """
+
+
+    x_train, x_test, d_train, d_test, y_train, y_test = split_data(treatment, outcome, covariates, percentage_test)
     model = generating_causal_forest(model_outcome, model_treatment, n_trees, min_sample_per_leaf, max_samples, random_seed, y_train, d_train, x_train)
 
     x_train_t = x_train.to_numpy()
