2877808185b9f17441ac25722b288c8d1c6e6df0
s33btorr <s33btorr@uni-bonn.de>
2025-07-23 14:40:31 +0200

    Add comments for execution in generating folder py.

--- ml_heterogeneity_project/final/generate_folders.py
+++ ml_heterogeneity_project/final/generate_folders.py
@@ -6,3 +6,7 @@ BLD = SRC / "bld"
 
 data_path.mkdir(parents=True, exist_ok=True)
 BLD.mkdir(parents=True, exist_ok=True)
+
+
+# After running this file, a folder call "bld" and a folder call "data" should be created.
+# You should upload the .dta in the data folder to continue running the project.
